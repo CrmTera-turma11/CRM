383a7ee22de2c17516a5180a8680514e78d36507
Eduarda Costa <eduarda59costa60@gmail.com>
2022-11-15 17:05:39 -0300

    botões

--- Js/routes.js
+++ Js/routes.js
@@ -7,3 +7,28 @@ function redirectCadastro(){
     const cadastroRedirect = document.getElementById('cadastroRedirect')
     cadastroRedirect.onclick = window.location.href = "../pages/telaCadastro.html"
 }
+
+function redirectTelaInicial() {
+    const telaInicial = document.getElementById('telaInicial')
+    telaInicial.onclick = window.location.href = "../pages/index.html"     
+}
+
+// function redirectTelaProdutos() {
+//     const telaProdutos = document.getElementById('telaProdutos')
+//     telaProdutos.onclick =window.location.href =
+// }
+
+// function redirectTelaPrecos() {
+//     const telaPrecos = document.getElementById('telaPrecos')
+//     telaPrecos.onclick =window.location.href =
+// }
+
+// function redirectTelaFerramentas() {
+//     const telaFerramentas = document.getElementById('telaFerramentas')
+//     telaFerramentas.onclick =window.location.href =
+// }
+
+// function redirectTelaEmpresa() {
+//     const telaEmpresa = document.getElementById('telaEmpresa')
+//     telaEmpresa.onclick =window.location.href =
+// }
